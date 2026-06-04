bb0bd8e62f7b0e3136a132708dc0143808fd41c9
Haryerit Larez <larezharyerit@gmail.com>
2026-06-04 04:20:43 -0400

    subcarpetas, estructura login y registro

---
+++ app/Views/Login/inicioSesion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesion</title>
+</head>
+
+    <main>
+    <body>
+      <h1>Bienvenido de vuelta!</h1>
+      <h4>Inicia sesion para continuar<h4>
+
+      <form action="" method="post">
+        <label for="Nombre de Usuario">Nombre de Usuario:</label> <br>
+        <input type="text" name="username" id="username" required> <br>
+        <br>
+        <label for="Contraseña">Contraseña:</label><br>
+        <input type="password" name="password" id="password" required>
+        <br>
+        <a href="">¿Olvidaste tu contraseña?</a><br>
+        <br>
+        <input type="submit" value="Iniciar Sesion">
+      </form>
+    </body>
+  </main>
+</html>
---
+++ app/Views/Registro/nuevoUsuario.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bienvenido a LAY-8 MAP</title>
+  </head>
+
+  <p>Breve descripcion de la aplicacion y sus funciones tsttsts nose
+  
+    <main>
+    <body>
+      <h1>LAY-8 MAP</h1>
+      <h2>Empieza Ahora!</h2>
+
+      <form action="" method="post">
+        <label for="username">Nombre de Usuario:</label>
+        <br />
+        <input type="text" id="username" name="username" required /><br /><br />
+        <label for="email">Correo Electrónico:</label>
+        <br />
+        <input type="email" id="email" name="email" required /><br /><br />
+        <input type="submit" value="Registrarse" />
+        <a href="">¿Ya tienes una cuenta? Inicia sesión</a>
+      </form>
+    </body>
+  </main>
+</html>
